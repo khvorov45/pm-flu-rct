797697a320dd7ba20a6b9012296bb8162fbec4aa
Arseniy Khvorov <khvorov45@gmail.com>
2020-05-21 12:36:53 +1000

    update lock with package

--- data/data.R
+++ data/data.R
@@ -1,3 +1,10 @@
 # Extract the data for analysis
 
 library(tidyverse)
+
+data_raw_dir <- here::here("data-raw")
+data_dir <- here::here("data")
+
+# Functions ===================================================================
+
+# Script ======================================================================
