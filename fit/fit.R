aba2878368f3f47d22c0502cb16b6e44498fd79b
Arseniy Khvorov <khvorov45@gmail.com>
2020-06-12 09:04:30 +1000

    add in ili model and table

--- fit/fit.R
+++ fit/fit.R
@@ -12,6 +12,13 @@ fit_dir <- here::here("fit")
 
 source(file.path(data_dir, "read_data.R"))
 
+fun_beta <- function(beta_name, fun_name) {
+  if (str_detect(beta_name, "\\{")) {
+    beta_name <- str_replace(beta_name, "\\{(.*)\\}", "\\{\\\\text\\{\\1\\}\\}")
+  }
+  paste0("$", fun_name, "(\\beta_", beta_name, ")$")
+}
+
 exp_beta <- function(beta_name) {
   if (str_detect(beta_name, "\\{")) {
     beta_name <- str_replace(beta_name, "\\{(.*)\\}", "\\{\\\\text\\{\\1\\}\\}")
@@ -38,19 +45,18 @@ fit_model <- function(data) {
 fit_model_ili <- function(data) {
   glm(
     ili ~ group +
-      +myeloma
+      myeloma
       + vac_in_prior_year
       + current_therapy
       + age_years_baseline_centered
-      + weeks4_since_tx_baseline_centered
-      + logtitre_baseline_centered,
+      + weeks4_since_tx_baseline_centered,
     binomial,
     data
   ) %>%
     broom::tidy()
 }
 
-gen_b0_int <- function() {
+gen_b0_int <- function(fits, what, when) {
   all <- list(
     "myeloma" = "cancer other than myeloma",
     "vac_in_prior_year" = "never vaccinated or vaccinated earlier than 2018",
@@ -62,13 +68,13 @@ gen_b0_int <- function() {
     "logtitre_baseline_centered" = "baseline titre of 5"
   )
   all <- all[names(all) %in% fits$term]
-  paste0(
-    "Expected titre for the standard dose group at visit 2, ",
+  glue::glue(
+    "Expected {what} for the standard dose group{when}, ",
     paste(all, collapse = ", "), "."
   )
 }
 
-adjusted <- function(this = NULL) {
+adjusted <- function(fits, this = NULL) {
   adj <- list(
     "myeloma" = "myeloma status",
     "vac_in_prior_year" = "previous vaccination status",
@@ -84,12 +90,148 @@ adjusted <- function(this = NULL) {
   paste0("Adjusted for ", paste(adj, collapse = ", "), ".")
 }
 
+gen_fits_ref <- function(fits, what_int, what_other, when_int, when_other,
+                         beta_fun_name, var_lbl_int, var_int_int) {
+  all <- tribble(
+    ~term, ~term_lbl, ~var_lbl, ~term_int, ~var_int,
+
+    "(Intercept)", fun_beta("0", beta_fun_name), var_lbl_int,
+    gen_b0_int(fits, what_int, when_int), var_int_int,
+
+    "groupHigh Dose", fun_beta("{HD}", beta_fun_name), "$G_H$",
+    glue::glue(
+      "Expected {what_other} for the high dose group",
+      " as compared to the standard dose group",
+      "{when_other}. ",
+      adjusted(fits)
+    ),
+    paste(
+      "Indicator of whether the observation belongs to the",
+      "high-dose group (1) or the standard-dose group (0)."
+    ),
+
+    "timepoint_lblVisit 3", fun_beta("{V3}", beta_fun_name), "$V_3$",
+    glue::glue(
+      "Expected {what_other} for either group",
+      " at visit 3 as compared to visit 2. ",
+      adjusted(fits)
+    ),
+    paste(
+      "Indicator of whether the observation belongs to",
+      "visit 3 (1) or not (0)."
+    ),
+
+    "timepoint_lblVisit 4", fun_beta("{V4}", beta_fun_name), "$V_4$",
+    glue::glue(
+      "Expected {what_other} for either group",
+      " at visit 4 as compared to visit 2. ",
+      adjusted(fits)
+    ),
+    paste(
+      "Indicator of whether the observation belongs to",
+      "visit 4 (1) or not (0)."
+    ),
+
+    "myeloma", fun_beta("M", beta_fun_name), "$M$",
+    glue::glue(
+      "Expected {what_other} for either group{when_other} ",
+      "for subjects with myeloma compared to subjects with other cancer type. ",
+      adjusted(fits, "myeloma")
+    ),
+    "Indicator of whether the subject has myeloma (1) or not (0)",
+
+    "vac_in_prior_year", fun_beta("{PV}", beta_fun_name), "$P$",
+    glue::glue(
+      "Expected {what_other} for either group{when_other} ",
+      "for subjects last vaccinated no earlier than 2018 ",
+      "compared to subjects last vaccinated earlier ",
+      "than 2018 or never vaccinated. ",
+      adjusted(fits, "vac_in_prior_year")
+    ),
+    paste(
+      "Indicator of whether the subject was vaccinated no earlier than 2018",
+      "(1) or either vaccinated earlier than 2018 or never vaccinated (0)."
+    ),
+
+    "current_therapy", fun_beta("{R}", beta_fun_name), "$R$",
+    glue::glue(
+      "Expected {what_other} for either group{when_other} ",
+      "for subjects receiving therapy as compared to subjects not ",
+      "receiving therapy. ",
+      adjusted(fits, "current_therapy")
+    ),
+    paste(
+      "Indicator of whether the subject was receiving therapy (1)",
+      "or not (0)."
+    ),
+
+    "age_years_centered", fun_beta("{AC}", beta_fun_name), "$A_C$",
+    glue::glue(
+      "Expected {what_other} for either group{when_other} ",
+      "for 1 year increase in age. ",
+      adjusted(fits, "age_years_centered")
+    ),
+    "Age in years at first visit. Centred on 50.",
+
+    "age_years_baseline_centered", fun_beta("{AC}", beta_fun_name), "$A_C$",
+    glue::glue(
+      "Expected {what_other} for either group{when_other} ",
+      "for 1 year increase in age. ",
+      adjusted(fits, "age_years_baseline_centered")
+    ),
+    "Age in years at time of measurement. Centred on 50.",
+
+    "weeks4_since_tx_centered", fun_beta("{XC}", beta_fun_name), "$X_C$",
+    glue::glue(
+      "Expected {what_other} for either group{when_other} ",
+      "for a 4-week increase in time from transplant. ",
+      adjusted(fits, "weeks4_since_tx_centered")
+    ),
+    paste(
+      "Number of 4-week periods (approximately months) from transplant ",
+      "to measurement. Centred on 1."
+    ),
+
+    "weeks4_since_tx_baseline_centered",
+    fun_beta("{XC}", beta_fun_name), "$X_C$",
+    glue::glue(
+      "Expected {what_other} for either group{when_other} ",
+      "for a 4-week increase in time from transplant. ",
+      adjusted(fits, "weeks4_since_tx_baseline_centered")
+    ),
+    paste(
+      "Number of 4-week periods (approximately months) from transplant",
+      "to first visit. Centred on 1."
+    ),
+
+    "logtitre_baseline_centered", fun_beta("{BC}", beta_fun_name), "$B_C$",
+    glue::glue(
+      "Expected {what_other} for either group{when_other} ",
+      " for a 2-fold increase in the baseline titre. ",
+      adjusted(fits, "logtitre_baseline_centered")
+    ),
+    paste(
+      "Log2-baseline titre (i.e. titre at visit 1) shifted to midpoint.",
+      "Centred on $\\text{log}_2(5)$."
+    ),
+
+    "sd_(Intercept).id", "$r_{\\text{Random}}$", "",
+    "A measure of between-subject variability.", "",
+
+    "sd_Observation.Residual", "$r_{\\text{Residual}}$", "",
+    "A measure of within-subject variability.", "",
+  )
+  filter(all, term %in% fits$term)
+}
+
 clean_label <- function(labels, patt = "\\\\text\\{exp\\}\\((.*)\\)") {
   str_replace_all(labels, "\\$", "") %>%
     str_replace_all(patt, "\\1")
 }
 
 # Script ======================================================================
+
+# Prepare data
 
 data <- read_data() %>%
   group_by(id, virus) %>%
@@ -105,165 +247,33 @@ data <- read_data() %>%
 data_reorg <- data %>%
   filter(timepoint != 1L)
 
-fits <- data_reorg %>%
+# Fit models
+
+fits_titre <- data_reorg %>%
   group_by(virus) %>%
   group_modify(~ fit_model(.x))
 
 fits_ili <- data %>%
   filter(timepoint == 1L) %>%
-  group_by(virus) %>%
-  group_modify(~ fit_model_ili(.x))
+  fit_model_ili()
 
-fits_ref <- tribble(
-  ~term, ~term_lbl, ~var_lbl, ~term_int, ~var_int,
+# Reference tables
 
-  "(Intercept)", exp_beta("0"), "$T_{l,m}$",
-  gen_b0_int(), "Natural log-titre shifted to the midpoint.",
-
-  "groupHigh Dose", exp_beta("{HD}"), "$G_H$",
-  paste(
-    "Expected fold-titre change for the high dose group",
-    "as compared to the standard dose group",
-    "at visits 2, 3 and 4.",
-    adjusted()
-  ),
-  paste(
-    "Indicator of whether the observation belongs to the",
-    "high-dose group (1) or the standard-dose group (0)."
-  ),
-
-  "timepoint_lblVisit 3", exp_beta("{V3}"), "$V_3$",
-  paste(
-    "Expected fold-titre change for either group",
-    "at visit 3 as compared to visit 2.",
-    adjusted()
-  ),
-  paste(
-    "Indicator of whether the observation belongs to",
-    "visit 3 (1) or not (0)."
-  ),
-
-  "timepoint_lblVisit 4", exp_beta("{V4}"), "$V_4$",
-  paste(
-    "Expected fold-titre change for either group",
-    "at visit 4 as compared to visit 2.",
-    adjusted()
-  ),
-  paste(
-    "Indicator of whether the observation belongs to",
-    "visit 4 (1) or not (0)."
-  ),
-
-  "myeloma", exp_beta("M"), "$M$",
-  paste(
-    "Expected fold-titre change for either group at visits 2, 3 and 4",
-    "for subjects with myeloma compared to subjects with other cancer type.",
-    adjusted("myeloma")
-  ),
-  "Indicator of whether the subject has myeloma (1) or not (0)",
-
-  "vac_in_prior_year", exp_beta("{PV}"), "$P$",
-  paste(
-    "Expected fold-titre change for either group at visits 2, 3 and 4",
-    "for subjects last vaccinated no earlier than 2018",
-    "compared to subjects last vaccinated earlier",
-    "than 2018 or never vaccinated.",
-    adjusted("vac_in_prior_year")
-  ),
-  paste(
-    "Indicator of whether the subject was vaccinated no earlier than 2018",
-    "(1) or either vaccinated earlier than 2018 or never vaccinated (0)."
-  ),
-
-  "current_therapy", exp_beta("{R}"), "$R$",
-  paste(
-    "Expected fold-titre change for either group at visits 2, 3 and 4",
-    "for subjects receiving therapy as compared to subjects not",
-    "receiving therapy.",
-    adjusted("current_therapy")
-  ),
-  paste(
-    "Indicator of whether the subject was receiving therapy (1)",
-    "or not (0)."
-  ),
-
-  "age_years_centered", exp_beta("{AC}"), "$A_C$",
-  paste(
-    "Expected fold-titre change for either group at visits 2, 3 and 4",
-    "for 1 year increase in age.",
-    adjusted("age_years_centered")
-  ),
-  "Age in years at first visit. Centred on 50.",
-
-  "age_years_baseline_centered", exp_beta("{AC}"), "$A_C$",
-  paste(
-    "Expected fold-titre change for either group at visits 2, 3 and 4",
-    "for 1 year increase in age.",
-    adjusted("age_years_baseline_centered")
-  ),
-  "Age in years at time of measurement. Centred on 50.",
-
-  "weeks4_since_tx_centered", exp_beta("{XC}"), "$X_C$",
-  paste(
-    "Expected fold-titre change for either group at visits 2, 3 and 4",
-    "for a 4-week increase in time from transplant.",
-    adjusted("weeks4_since_tx_centered")
-  ),
-  paste(
-    "Number of 4-week periods (approximately months) from transplant",
-    "to measurement. Centred on 1."
-  ),
-
-  "weeks4_since_tx_baseline_centered", exp_beta("{XC}"), "$X_C$",
-  paste(
-    "Expected fold-titre change for either group at visits 2, 3 and 4",
-    "for a 4-week increase in time from transplant.",
-    adjusted("weeks4_since_tx_baseline_centered")
-  ),
-  paste(
-    "Number of 4-week periods (approximately months) from transplant",
-    "to first visit. Centred on 1."
-  ),
-
-  "logtitre_baseline_centered", exp_beta("{BC}"), "$B_C$",
-  paste(
-    "Expected fold-titre change for either group at visits 2, 3 and 4",
-    "for a 2-fold increase in the baseline titre.",
-    adjusted("logtitre_baseline_centered")
-  ),
-  paste(
-    "Log2-baseline titre (i.e. titre at visit 1) shifted to midpoint.",
-    "Centred on $\\text{log}_2(5)$."
-  ),
-
-  "sd_(Intercept).id", "$r_{\\text{Random}}$", "",
-  "A measure of between-subject variability.", "",
-
-  "sd_Observation.Residual", "$r_{\\text{Residual}}$", "",
-  "A measure of within-subject variability.", "",
+fits_ref_titre <- gen_fits_ref(
+  fits_titre, "titre", "fold-titre change", " at visit 2", " at any visit",
+  "\\text{exp}", "$T_{l,m}$",
+  "Natural log-titre shifted to the midpoint."
+)
+fits_ref_ili <- gen_fits_ref(
+  fits_ili, "odds of infection", "odds ratio of infection", "", "",
+  "\\text{logit}^{-1}", "$I$",
+  "Indicator of infection (1) or no infection (0) during the study period"
 )
 
 write_csv(
-  left_join(fits, fits_ref, by = "term"),
+  left_join(fits_titre, fits_ref_titre, by = "term"),
   file.path(fit_dir, "fits-titre.csv")
 )
-
-fits_ref_ili <- fits_ref %>%
-  mutate(
-    term_lbl = str_replace(term_lbl, "\\{exp\\}", "\\{logit\\}\\^\\{-1\\}"),
-    term_int = str_replace(
-      term_int, "fold-titre change", "odds ratio of infection"
-    ) %>%
-      str_replace("Expected titre", "Expected log-odds of infection") %>%
-      str_replace(" at visit 2", "") %>%
-      str_replace(" at visits 2, 3 and 4", ""),
-    var_lbl = if_else(term == "(Intercept)", "$I$", var_lbl),
-    var_int = if_else(
-      term == "(Intercept)",
-      "Indicator of infection (1) or no infection (0) during the study period",
-      var_int
-    )
-  )
 
 write_csv(
   left_join(fits_ili, fits_ref_ili, by = "term"),
@@ -271,7 +281,7 @@ write_csv(
 )
 
 fits_ref_all <- list(
-  "titre" = fits_ref %>% filter(term %in% fits$term),
+  "titre" = fits_ref_titre %>% filter(term %in% fits$term),
   "ili" = fits_ref_ili %>% filter(term %in% fits_ili$term)
 )
 
@@ -322,7 +332,7 @@ iwalk(fits_ref_all, function(fits_ref_rel, name) {
     kable(
       format = "latex",
       caption = "Interpretation of the model parameters",
-      label = "estimates-interpret",
+      label = glue::glue("estimates-interpret-{name}"),
       escape = FALSE,
       booktabs = TRUE,
       align = "ll"
