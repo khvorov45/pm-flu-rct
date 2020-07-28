badfc4b8664ffc1115e012de4e2eae4b564f9482
Arseniy Khvorov <khvorov45@gmail.com>
2020-07-29 08:13:21 +1000

    add sheena's plot

--- data-plot/data-plot.R
+++ data-plot/data-plot.R
@@ -35,7 +35,7 @@ make_plot <- function(data, data_summ) {
       legend.position = "bottom",
       legend.box.spacing = unit(0, "null"),
     ) +
-    scale_y_log10("HI Titre", breaks = 5 * 2^(0:11)) +
+    scale_y_log10("HI Titre", breaks = 5 * 2^(0:11)) + # nolint
     scale_x_continuous("Days since first visit") +
     scale_color_discrete("Timepoint") +
     scale_shape_discrete("Timepoint") +
@@ -49,6 +49,57 @@ make_plot <- function(data, data_summ) {
       inherit.aes = FALSE
     ) +
     guides(color = guide_legend(override.aes = list(alpha = 1)))
+}
+
+make_plot_sheena <- function(data, data_summ, nrow = 2) {
+  timepoints <- data %>%
+    group_by(timepoint_lbl) %>%
+    summarise(
+      mean_days_since_t1 = mean(days_since_t1),
+      .groups = "drop"
+    )
+
+  data_sheena <- inner_join(
+    data, data_summ,
+    by = c("virus", "timepoint_lbl", "group")
+  ) %>%
+    select(-mean_days_since_t1) %>%
+    inner_join(timepoints, by = c("timepoint_lbl"))
+
+  data_sheena %>%
+    mutate(
+      # This is ridiculous
+      mean_days_since_t1 = if_else(
+        group == "HD-SD", mean_days_since_t1 + 5, mean_days_since_t1 - 5
+      ),
+      mean_days_since_t1_rand = rnorm(n(), mean_days_since_t1, 1.5),
+      titre_rand = exp(rnorm(n(), log(titre), 0.1))
+    ) %>%
+    ggplot(aes(mean_days_since_t1_rand, titre_rand, col = group)) +
+    ggdark::dark_theme_bw(verbose = FALSE) +
+    theme(
+      strip.background = element_blank(),
+      panel.grid.minor = element_blank(),
+      legend.position = "bottom",
+      legend.box.spacing = unit(0, "null"),
+      axis.text.x = element_text(angle = 45, hjust = 1)
+    ) +
+    scale_y_log10("HI Titre", breaks = 5 * 2^(0:11)) + # nolint
+    scale_x_continuous(
+      breaks = timepoints$mean_days_since_t1, labels = timepoints$timepoint_lbl
+    ) +
+    scale_color_discrete("Group") +
+    scale_linetype_discrete("Group") +
+    xlab("Timepoint") +
+    facet_wrap(~virus) +
+    geom_line(aes(group = id), alpha = 0.1) +
+    geom_point(size = 0.2, alpha = 0.8) +
+    geom_line(
+      data = data_summ,
+      aes(mean_days_since_t1, titre_geom_mean, col = group),
+      lwd = 1,
+      inherit.aes = FALSE
+    )
 }
 
 save_plot <- function(plot, name, width, heigth) {
@@ -101,3 +152,12 @@ seroconverted_combined_plot <- seroconverted_combined %>%
   geom_errorbar(aes(ymin = prop_low, ymax = prop_high), width = 0.5) +
   geom_point(size = 4)
 save_plot(seroconverted_combined_plot, "seroconverted-nprot", 12, 7)
+
+# Sheena's "plot something like this"
+spag_sheena <- make_plot_sheena(data, data_summ)
+save_plot(spag_sheena, "spag-sheena", 15, 15)
+
+spag_sheena_nobvic <- data %>%
+  filter(virus != "B Vic") %>%
+  make_plot_sheena(filter(data_summ, virus != "B Vic"), 1)
+save_plot(spag_sheena_nobvic, "spag-sheena-nobvic", 17, 10)
